bab98248ef8bea34cb3830d1dc4ed445fe598a28
ranchocooper <ranchocooper@gmail.com>
2016-12-23 19:37:58 +0800

    domain划分

---
+++ Algorithms/compare-the-triplets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author   : Rancho Cooper
+# @date     : 2016-12-07 10:30
+# @email    : ranchocooper@gmail.com
+
+al, bo = 0, 0
+
+for i in range(2):
+
--- Python/itertools/itertools-product.py
+++ Python/itertools/itertools-product.py
@@ -6,7 +6,7 @@
 
 from itertools import product
 
-a = list(map(int, input.split()))
-b = list(map(int, input.split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
 print(*list(product(a, b)))
