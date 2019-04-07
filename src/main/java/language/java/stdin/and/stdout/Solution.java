dc0e1b4d7be29d075028ae806926c70c9610b045
Rancho <ranchocooper@gmail.com>
2019-04-07 21:25:03 +0800

    merge java project to repo

---
+++ src/main/java/language/java/stdin/and/stdout/Solution.java
@@ -0,0 +1,8 @@
+package language.java.stdin.and.stdout;
+
+/**
+ * @author rancho
+ * @date 2019-03-30
+ */
+public class Solution {
+}
---
+++ src/main/java/language/java/welcome/Solution.java
@@ -0,0 +1,12 @@
+package language.java.welcome;
+
+/**
+ * @author rancho
+ * @date 2019-03-30
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
