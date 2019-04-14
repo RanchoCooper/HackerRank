d644830af75ac9e65452df76bc7a43cce1d3ed82
Rancho <ranchocooper@gmail.com>
2019-04-14 16:09:39 +0800

    up

--- src/main/java/language/java/stdin/and/stdout/Solution.java
+++ src/main/java/language/java/stdin/and/stdout/Solution.java
@@ -1,8 +1,29 @@
 package language.java.stdin.and.stdout;
 
+import java.util.Scanner;
+
 /**
  * @author rancho
  * @date 2019-03-30
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // String myString = scanner.next();
+        // int myInteger = scanner.nextInt();
+        // scanner.close();
+        //
+        // System.out.println("got string: " + myString);
+        // System.out.println("got integer: " + myInteger);
+
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 }
